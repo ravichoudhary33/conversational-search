4410531aed68c3a1096129415e21bbf94550e3fc
ayushkedia29 <ayush.bimal@unbxd.com>
2023-06-09 13:27:49 +0530

    refactor: remove merge conflicts

--- alchemist/main.py
+++ alchemist/main.py
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-from fastapi import FastAPI, UploadFile, Request
-from pydantic import BaseModel
-from PIL import Image
-from io import BytesIO
-from fastapi.middleware.cors import CORSMiddleware
-from agents import create_agent, tools, llm
-from chat import chat
-=======
 from fastapi import FastAPI, Request, Response
 import datetime
 from pydantic import BaseModel, parse_obj_as
@@ -21,112 +12,10 @@ REDIS_ENTRIES = 3
 from downstream import reranker_client, brewer_client, top_query_brewer_client
 
 REDIS_HOST = 'redis'
->>>>>>> 2bba826 (fix: backend complete implementation)
 
 app = FastAPI()
 
 
-<<<<<<< HEAD
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=['*']
-)
-
-origins = ["*"]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-convo_history = {}
-
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-class TextRequest(BaseModel):
-    text: str
-    convo_id: str
-
-class Product(BaseModel):
-    productId: int
-    productName: str
-    imgUrl: str
-    price: float
-
-class TextResponse(BaseModel):
-    text: str
-    filter: list[str]
-    Products: list[Product]
-    convo_id: str
-
-@app.post("/sites/{sitekey}/chatbot", response_model=TextResponse)
-def get_text_chat_response(sitekey,request: TextRequest,query_request : Request):
-    # Process the request and generate the response
-    query_params = query_request.query_params
-    userid = query_params.get('uid')
-
-
-    if userid in convo_history:
-        langchain_info = convo_history[userid]
-    else:
-        langchain_info = create_agent(llm, tools)
-        convo_id = "1234567891234567"
-        convo_history[userid] = langchain_info
-
-    text, filter_options, products = chat(langchain_info, request.text)
-    print(text, filter_options, products)
-
-    # text = "Sure, here are a few shirts for "+userid+" "+ request.convo_id + " "+ sitekey
-    filter_options = ["hrx", "Raymond", "Below 5K"]
-    # if(request.text.lower() =="hello"):
-    #     text = "Hello how are you"
-    #     filter_options = ["Loui phillip", "Levis", "Above 5K",'Below 2k','red']
-    # if(request.text.lower() =="hi"):
-    #     text = "Hola!!!"
-    #     filter_options = ["Raymond", "Pepe", "Blue",'Above 1K']
-    products = [
-        Product(productId=123, productName="Loui Phillip Shirt", imgUrl="https://picsum.photos/200/300",price=2400),
-        Product(productId=456, productName="One Shirt", imgUrl="https://picsum.photos/200/300",price=2500),
-        Product(productId=456, productName="Two Shirt", imgUrl="https://picsum.photos/200/300",price=2800),
-        Product(productId=456, productName="Two Shirt", imgUrl="https://picsum.photos/200/300",price=2800),
-        Product(productId=456, productName="Two Shirt", imgUrl="https://picsum.photos/200/300",price=2800),
-        Product(productId=456, productName="Two Shirt", imgUrl="https://picsum.photos/200/300",price=2800),
-        # Add more product objects as needed
-    ]
-
-    response = TextResponse(text=text, filter=filter_options, Products=products,convo_id=request.convo_id)
-    return response
-
-
-class ImageRequest(BaseModel):
-    image: UploadFile
-
-
-
-@app.post("/api/getImageResponse", response_model=TextResponse)
-def get_image_chat_response(request: TextRequest):
-    # Process the image and generate the response
-    image = Image.open(BytesIO(request.image.file.read()))
-    # Perform necessary image processing or analysis on 'image'
-
-    text = "Sure, here are a few shirts"
-    filter_options = ["hrx", "Raymond", "Below 5K"]
-    products = [
-        Product(productId=123, productName="Loui Phillip Shirt", imgUrl="https://picsum.photos/200/300"),
-        Product(productId=456, productName="Another Shirt", imgUrl="https://picsum.photos/200/300"),
-        # Add more product objects as needed
-    ]
-
-    response = TextResponse(text=text, filter=filter_options, Products=products)
-    return response
-=======
 def collect_message(userId, text):
     return ("123123","Bye", [],[],[])
 
@@ -346,4 +235,3 @@ def add_convo_id(val, convo_id, data):
     val.update(convo_data)
     return val
 
->>>>>>> 2bba826 (fix: backend complete implementation)
